4faef8b4ee00dedce64e78b4928b5a5bfac50f47
Bobby-H <imbobbyhutter@gmail.com>
2022-01-19 21:22:45 -0500

    added greeting and landmark list

--- skyroute.py
+++ skyroute.py
@@ -4,3 +4,17 @@ from vc_landmarks import vc_landmarks
 from landmark_choices import landmark_choices
 
 # Build your program below:
+landmark_string = ""
+def greet():
+  print('''\n Hi there and welcome to SkyRoute! \n 
+  We'll help you find the shortest route betwen 
+  the following Vancouver landmarks: \n''' + landmark_string)
+
+def skyroute():
+  greet()
+
+for letter, landmark in landmark_choices.items():
+  landmark_string += "{0} - {1}\n".format(letter, landmark)
+print(landmark_string)
+
+skyroute()
